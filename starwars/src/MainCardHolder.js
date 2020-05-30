0d335c32cd2a6cc9e519717056eef2a273f28d93
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-05-29 23:18:49 -0500

    added style to character cards for mobile

--- starwars/src/MainCardHolder.js
+++ starwars/src/MainCardHolder.js
@@ -14,6 +14,10 @@ const Card = styled.div`
   background-color: #eee;
   width: 25%;
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
+  @media (max-width: 768px) {
+    width: 70%;
+  }
 `;
 
 const Name = styled.h2`
